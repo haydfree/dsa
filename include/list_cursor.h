e74ec89f3d1e74bf854d9dabf2c1b977060580e1
Hayden Freeman <haydenfreeman@yahoo.com>
2025-06-06 09:44:46 -0500

    AUTO COMMIT: 2025-06-06 09:44:46

--- include/list_cursor.h
+++ include/list_cursor.h
@@ -5,30 +5,23 @@
 #include <stdlib.h>
 #include "defines.h"
 
-#define MAX_LCNODES 256
+#define LC_POOL_SIZE 256
 
 typedef struct dsa_LCNode
 {
 	void *data;
-	u32 index, next;
-	b8 used;
+	struct dsa_LCNode *next;
 } dsa_LCNode;
 
 typedef struct dsa_LC
 {
-	struct dsa_LCNode pool[MAX_LCNODES];
-	u32 head, size;
+	dsa_LCNode *head;
 } dsa_LC;
 
-i8 dsa_lc_push_front(dsa_LC * const lc, dsa_LCNode * const node);
-i8 dsa_lc_push_back(dsa_LC * const lc, dsa_LCNode * const node);
-i8 dsa_lc_pop_front(dsa_LC * const lc);
-i8 dsa_lc_pop_back(dsa_LC * const lc);
-i8 dsa_lc_get_next(const dsa_LC * const lc, const dsa_LCNode * const node, const dsa_LCNode *dst);
-i8 dsa_lc_get_prev(const dsa_LC * const lc, const dsa_LCNode * const node, const dsa_LCNode *dst);
-i8 dsa_lc_get_head(const dsa_LC * const lc, const dsa_LCNode **dst);
-i8 dsa_lc_get_tail(const dsa_LC * const lc, const dsa_LCNode **dst);
-i8 dsa_lc_print(const dsa_LC * const lc);
-i8 dsa_lc_clear(dsa_LC * const lc);
+typedef struct dsa_LCPool
+{
+	dsa_LCNode pool[LC_POOL_SIZE];
+	dsa_LCNode *ptr;
+} dsa_LCPool;
 
 #endif
--- test/test.c
+++ test/test.c
@@ -1,37 +1,8 @@
 #include "test.h"
 
-void
-test_lc_get_head(void)
-{
-	dsa_LC lc = {0};	
-	dsa_LCNode head = {0}, *tmp = NULL;
-
-	head.index = 69;
-	head.used = TRUE;
-	lc.head = head.index;
-
-	assert(dsa_lc_get_head(&lc, &tmp) == EXIT_SUCCESS);
-	assert(*tmp == head);
-}
-
-void
-test_lc_get_tail(void)
-{
-	dsa_LC lc = {0};	
-}
-
-void 
-test_lc_push_front(void)
-{
-	dsa_LC lc = {0};	
-
-}
-
 int
 main(void)
 {
-	test_lc_get_head();
-	test_lc_get_tail();
 
 	printf("All tests passed!\n");
 	return 0;
