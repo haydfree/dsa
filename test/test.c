cca26c0a7684d63973c07f873900bb0f36a1143b
Hayden Freeman <haydenfreeman@yahoo.com>
2025-06-08 09:34:10 -0500

    AUTO COMMIT: 2025-06-08 09:34:10

--- test/test.c
+++ test/test.c
@@ -237,7 +237,7 @@ test_lc_push_front(void)
 	setup_list_fixture(&fixture);
 	fill_fixture_pool(&fixture);
 	TEST_DUMP_LIST_STATE(&fixture.list);
-	TEST_ASSERT(dsa_lc_push_front(&fixture.list, (const void*)0x01) == EXIT_SUCCESS, "Pushing front should succeed with full pool");
+	TEST_ASSERT(dsa_lc_push_front(&fixture.list, (const void*)0x01) == EXIT_FAILURE, "Pushing front should fail with full pool");
 	TEST_DUMP_LIST_STATE(&fixture.list);
 	teardown_list_fixture(&fixture);
 
