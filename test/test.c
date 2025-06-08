fd183c2b40d59688f16eee3820a4a9157888556b
Hayden Freeman <haydenfreeman@yahoo.com>
2025-06-08 09:25:30 -0500

    AUTO COMMIT: 2025-06-08 09:25:30

--- test/test.c
+++ test/test.c
@@ -193,6 +193,13 @@ test_lc_push_front(void)
 	teardown_list_fixture(&fixture);
 
 	// min data
+	setup_list_fixture(&fixture);
+	TEST_DUMP_LIST_STATE(&fixture.list);
+	TEST_ASSERT(dsa_lc_push_front(&fixture.list, (const void*)0x00) == EXIT_SUCCESS, "Pushing front should succeed with min data");
+	TEST_DUMP_LIST_STATE(&fixture.list);
+	teardown_list_fixture(&fixture);
+
+	// min *data
 	
 
 	// full list
